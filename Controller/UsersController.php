02c90154b33f181b5268b70f73f98547a799df11
杉山佑里子 <peaceful.future1101@gmail.com>
2016-08-05 14:37:31 +0900

    バックアップ用コミット

--- Controller/UsersController.php
+++ Controller/UsersController.php
@@ -3,12 +3,16 @@
 class UsersController extends AppController {
 
 	public function index() {
+		//usersテーブルから全てのデータをさらってUserモデルを経由してビューに表示する
 		$this->set('users', $this->User->find('all'));
 	}
 
 	public function add_step1() {
+		//フォームが送信されたら
 		if ($this->request->is('post')) {
+			//Userモデルクラスから新規
 			$this->User->create();
+			//Userモデルからバリデーションをチェックしにいく
 			if ($this->User->validates()) {
 				// 正しい場合のロジック
 				return $this->redirect(['action' => 'add_step3']);
